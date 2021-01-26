b6ce3caa1d85a4e7fcd0ce0e59e4c8234f16a32e
nilsbuehrer <nils@ception.ch>
2021-01-26 19:52:49 +0100

    Updates

---
+++ precache-manifest.014679156ff86767bdc7ff98ef3ee87e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "07c3475ec21338d6875f017d2231fdf4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "348af348d03a54224fac",
+    "url": "/static/css/main.0b4d1463.chunk.css"
+  },
+  {
+    "revision": "99c2efe5963ac632a063",
+    "url": "/static/js/2.2e02ae8d.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/static/js/2.2e02ae8d.chunk.js.LICENSE"
+  },
+  {
+    "revision": "348af348d03a54224fac",
+    "url": "/static/js/main.5100b19e.chunk.js"
+  },
+  {
+    "revision": "9169960c70adfffb1727",
+    "url": "/static/js/runtime-main.019720d1.js"
+  }
+]);
