31c576a547ada4754061f3c272019ea8b9c5fe21
nilsbuehrer <nils@ception.ch>
2021-01-27 08:41:04 +0100

    add animation

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
 import React, { useState } from "react";
-import "./App.css";
+import "./App.scss";
 import { ThemeProvider } from "styled-components";
 import { lightTheme, darkTheme } from "./theme";
 import { GlobalStyles } from "./global";
+import Title from './Title/Title';
 
 function App() {
   const [theme, setTheme] = useState("light");
@@ -16,10 +17,11 @@ function App() {
   };
   return (
     <ThemeProvider theme={theme === 'light' ? lightTheme : darkTheme}>
+      <link href="https://fonts.googleapis.com/css?family=Material+Icons|Material+Icons+Outlined|Material+Icons+Two+Tone|Material+Icons+Round|Material+Icons+Sharp" rel="stylesheet"></link>
       <GlobalStyles />
       <div className="App" onClick={toggleTheme}>
         <header className="App-header">
-          <h1>cept.ch</h1>
+          <Title />
           <span>by Nils Bührer</span>
         </header>
       </div>
---
+++ src/Title/Title.js
@@ -0,0 +1,20 @@
+import React, { Component } from "react";
+import "./Title.scss"
+
+export class Title extends Component {
+  render() {
+    return (
+      <h1>
+        <span>c</span>
+        <span>e</span>
+        <span>p</span>
+        <span>t</span>
+        <span>.</span>
+        <span>c</span>
+        <span>h</span>
+      </h1>
+    );
+  }
+}
+
+export default Title;
